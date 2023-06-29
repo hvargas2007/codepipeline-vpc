caa4953128de61d735744d537472a3a8de2daa85
vargascloudh <hermes.vargas@cloudhesive.comgit config --global user.name vargascloudh>
2023-06-29 19:28:23 -0300

    tfsec

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -3,6 +3,8 @@ resource "aws_cloudwatch_log_group" "vpc_log_group" {
   name              = "${var.name_prefix}-VpcLogGroup"
   retention_in_days = var.logs_retention
 
+  #tfsec:ignore:aws-cloudwatch-log-group-customer-key  
+
   tags = { Name = "${var.name_prefix}-vpc-Logs" }
 }
 
--- iam.tf
+++ iam.tf
@@ -10,7 +10,8 @@ data "aws_iam_policy_document" "vpc_fl_policy_source" {
       "logs:DescribeLogGroups",
       "logs:DescribeLogStreams"
     ]
-    resources = ["*"]
+    #tfsec:ignore:aws-iam-no-policy-wildcards
+    resources = ["*"]   
   }
 }
 
