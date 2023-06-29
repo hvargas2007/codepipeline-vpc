4f51b5585c9aa3c6112e1fda17a13618cbc29d40
vargascloudh <hermes.vargas@cloudhesive.comgit config --global user.name vargascloudh>
2023-06-29 19:37:16 -0300

    tfsecsg

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,9 +1,7 @@
 ##CloudWatch log group Produccion [3 days retention]
-resource "aws_cloudwatch_log_group" "vpc_log_group" {
+resource "aws_cloudwatch_log_group" "vpc_log_group" { #tfsec:ignore:aws-cloudwatch-log-group-customer-key
   name              = "${var.name_prefix}-VpcLogGroup"
   retention_in_days = var.logs_retention
-
-  #tfsec:ignore:aws-cloudwatch-log-group-customer-key  
 
   tags = { Name = "${var.name_prefix}-vpc-Logs" }
 }
